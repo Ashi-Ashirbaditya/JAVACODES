60c037ef3da5f0b8d2f735674cd85a77f93cbce9
Ashi-Ashirbaditya <129829025+Ashi-Ashirbaditya@users.noreply.github.com>
2023-05-04 11:35:35 +0530

    if exampls
    code of the if example

---
+++ ifexample.java.java
@@ -0,0 +1,12 @@
+class ifexample
+{
+    public static void main(String args[])
+    {
+       int a=10;
+       if(a<20)
+       {
+        System.out.println("yup a is less than 20");
+
+       }
+    }
+}
